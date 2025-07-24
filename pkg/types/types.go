62e1c7fe7f5fb49c899017f1d23bbd1593f5159a
Matías Insaurralde <matias@insaurral.de>
2025-07-24 19:30:48 -0300

    Fix types and golangci-lint version
    Signed-off-by: Matías Insaurralde <matias@insaurral.de>

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,26 +1,40 @@
 // Package types provides common data structures for the Nina application.
 package types
 
+// DeploymentStatus represents the status of a deployment.
+type DeploymentStatus string
+
+const (
+	// DeploymentStatusUnavailable represents a deployment that is unavailable.
+	DeploymentStatusUnavailable DeploymentStatus = "unavailable"
+	// DeploymentStatusDeploying represents a deployment that is deploying.
+	DeploymentStatusDeploying DeploymentStatus = "deploying"
+	// DeploymentStatusReady represents a deployment that is ready.
+	DeploymentStatusReady DeploymentStatus = "ready"
+)
+
 // Deployment represents a deployment configuration.
 type Deployment struct {
-	AppName string `json:"app_name"`
-	RepoURL string `json:"repo_url"`
-}
-
-// DeploymentBuildRequest represents a request to build a deployment.
-type DeploymentBuildRequest struct {
-	AppName       string `json:"app_name"`
-	RepoURL       string `json:"repo_url"`
-	Author        string `json:"author"`
-	AuthorEmail   string `json:"author_email"`
-	CommitHash    string `json:"commit_hash"`
-	NoContainers  int64  `json:"no_containers"`
-	Status        string `json:"status"`
-	BundleContent string `json:"bundle_content"`
+	AppName    string           `json:"app_name"`
+	RepoURL    string           `json:"repo_url"`
+	Containers []Container      `json:"containers"`
+	Status     DeploymentStatus `json:"status"`
 }
 
 // Container represents a container configuration.
 type Container struct {
 	ContainerID string `json:"container_id"`
 	ImageTag    string `json:"image_tag"`
+}
+
+// DeploymentBuildRequest represents a request to build a deployment.
+type DeploymentBuildRequest struct {
+	AppName        string `json:"app_name"`
+	RepoURL        string `json:"repo_url"`
+	Author         string `json:"author"`
+	AuthorEmail    string `json:"author_email"`
+	CommitHash     string `json:"commit_hash"`
+	NoContainers   int64  `json:"no_containers"`
+	Status         string `json:"status"`
+	BundleContents string `json:"bundle_content"`
 }
